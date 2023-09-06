212bc115e2032b33efa554ec79302a415ffb1bea
a1111 <okhrymovych11@gmail.com>
2023-09-06 18:44:09 +0300

    Commit to Spring Boot Data Validation Exception Handler Branch Second

--- src/main/java/mate/academy/intro/exception/EntityNotFoundException.java
+++ src/main/java/mate/academy/intro/exception/EntityNotFoundException.java
@@ -1,13 +1,7 @@
 package mate.academy.intro.exception;
 
-import java.util.function.Supplier;
-
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
-    }
-
-    public static Supplier<EntityNotFoundException> supplier(String message) {
-        return () -> new EntityNotFoundException(message);
     }
 }
