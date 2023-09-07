fac36d5011c2048fa985979fcf769b8487cf92a9
a1111 <okhrymovych11@gmail.com>
2023-09-07 14:07:00 +0300

    Commit to Spring Boot Pagination, Sorting, and Swagger Branch First

--- src/main/java/mate/academy/intro/controller/BookController.java
+++ src/main/java/mate/academy/intro/controller/BookController.java
@@ -1,12 +1,16 @@
 package mate.academy.intro.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.intro.dto.BookDto;
 import mate.academy.intro.dto.BookSearchParametersDto;
 import mate.academy.intro.dto.CreateBookRequestDto;
 import mate.academy.intro.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/books")
@@ -26,18 +31,28 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> findAll() {
-        return bookService.getAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookService.getAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book by id", description = "Get a book by id")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     @GetMapping("/author")
+    @Operation(summary = "Get all books by author", description = "Get a list of all books by author")
     public List<BookDto> getAllByAuthor(@RequestParam String author) {
         return bookService.getAllByAuthor(author);
+    }
+
+    @GetMapping("/price")
+    @Operation(summary = "Get all books by price, pagination and sorting")
+    public List<BookDto> findAllByPrice(@RequestParam BigDecimal from,
+                                           @RequestParam BigDecimal to, Pageable pageable) {
+        return bookService.getAllByPriceBetween(from, to, pageable);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -58,6 +73,8 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new book", description = "Create a new book")
     public BookDto save(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
--- src/main/java/mate/academy/intro/repository/book/BookRepository.java
+++ src/main/java/mate/academy/intro/repository/book/BookRepository.java
@@ -1,10 +1,14 @@
 package mate.academy.intro.repository.book;
 
+import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.intro.model.Book;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     List<Book> findAllByAuthorContainingIgnoreCase(String author);
+
+    List<Book> findAllByPriceBetween(BigDecimal from, BigDecimal to, Pageable pageable);
 }
