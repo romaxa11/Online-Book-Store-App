cca608d5d39cac2822385a3db6e78b586d407655
a1111 <okhrymovych11@gmail.com>
2023-09-25 13:56:45 +0300

    Commit to spring-boot-practice-order-branch Second

--- src/main/java/mate/academy/intro/model/Order.java
+++ src/main/java/mate/academy/intro/model/Order.java
@@ -18,11 +18,9 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
